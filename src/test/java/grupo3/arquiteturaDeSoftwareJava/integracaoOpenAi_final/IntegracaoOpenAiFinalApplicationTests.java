227152b5f0658282108e23487bedde5f60f7e014
Thiago Martins <thiagolealmartins@gmail.com>
2024-03-16 16:56:01 -0300

    primeiro commit

---
+++ src/test/java/grupo3/arquiteturaDeSoftwareJava/integracaoOpenAi_final/IntegracaoOpenAiFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3.arquiteturaDeSoftwareJava.integracaoOpenAi_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoOpenAiFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
