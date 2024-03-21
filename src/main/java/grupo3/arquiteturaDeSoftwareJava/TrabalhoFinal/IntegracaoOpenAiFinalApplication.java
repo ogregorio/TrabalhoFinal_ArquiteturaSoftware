1e7ab11a2925beed47041a4ae69b31db34647842
gregorio <arthurgregorioleal@gmail.com>
2024-03-21 19:11:45 -0300

    fix: melhorias no boot da aplicação

--- src/main/java/grupo3/arquiteturaDeSoftwareJava/TrabalhoFinal/IntegracaoOpenAiFinalApplication.java
+++ src/main/java/grupo3/arquiteturaDeSoftwareJava/TrabalhoFinal/IntegracaoOpenAiFinalApplication.java
@@ -6,12 +6,14 @@ import grupo3.arquiteturaDeSoftwareJava.TrabalhoFinal.service.OpenAIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
 import java.util.Scanner;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, WebMvcAutoConfiguration.class })
 public class IntegracaoOpenAiFinalApplication implements CommandLineRunner {
 	@Autowired
 	private final FunctionExecutorService functionService;
@@ -26,7 +28,9 @@ public class IntegracaoOpenAiFinalApplication implements CommandLineRunner {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntegracaoOpenAiFinalApplication.class, args);
+		SpringApplication application = new SpringApplication(IntegracaoOpenAiFinalApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
 
 	@Override
